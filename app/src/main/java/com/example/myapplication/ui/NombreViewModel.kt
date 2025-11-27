620cd0f10b693abfb9d187cb0fc4013652f2e336
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-11-27 12:04:49 -0300

    MVVM simple finalizado

--- app/src/main/java/com/example/myapplication/ui/NombreViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/NombreViewModel.kt
@@ -2,12 +2,24 @@ package com.example.myapplication.ui
 
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.data.datos
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class NombreViewModel : ViewModel() {
 
-    private fun elegirNombreAlAzar(): String {
-        // Continue picking up a new random word until you get one that hasn't been used before
-        return datos.random()
+    // Nombre UI Estado
+
+    //Creo un MutableStateFlow que es el flujo de  datos que puede cambiar
+        private val _uiEstado = MutableStateFlow(NombreEstado())
+
+    //Convierto el flujo de arriba en un StateFlow que no puede cambiar
+        val uiEstado: StateFlow<NombreEstado> = _uiEstado.asStateFlow()
+
+
+    fun elegirNombreAlAzar() {
+
+        _uiEstado.value = NombreEstado(datos.random())
 
     }
 
--- app/src/main/java/com/example/myapplication/ui/Pantalla.kt
+++ app/src/main/java/com/example/myapplication/ui/Pantalla.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication.ui.theme
+package com.example.myapplication.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -11,23 +12,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.ui.NombreViewModel
 
 
-@Composable
-fun Pantalla() {
-    val nombreUiEstado by NombreViewModel.uiState.collectAsState()
-    val nombre = nombreUiEstado.nombre
 
-    Column(modifier = Modifier.fillMaxWidth(),
+@Composable
+fun Pantalla(nombreViewModel: NombreViewModel = viewModel()) {
+
+    val nombreUiEstado by nombreViewModel.uiEstado.collectAsState()
+  //  val nombre = nombreUiEstado.nombre
+
+    Column(modifier = Modifier.fillMaxWidth(). fillMaxHeight(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
         Card(modifier = Modifier.padding(10.dp)) {
-            Text(text = "Hola!!!!", modifier = Modifier.padding(10.dp))
+
+            Text(text = nombreUiEstado.nombre, modifier = Modifier.padding(10.dp))
         }
-        OutlinedButton(onClick = { elegirNombreAlAzar()}) {
+        OutlinedButton(onClick = { nombreViewModel.elegirNombreAlAzar() }) {
             Text(text = "Cambiar nombre")
         }
 
